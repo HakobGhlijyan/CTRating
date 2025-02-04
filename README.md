ecc7f22d339b1fb8d025b903cee1d711a891cc9c
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2025-02-04 15:09:46 +0400

    Add custom verion SF symbol

---
+++ README.md
@@ -0,0 +1,3 @@
+# CTRating
+
+This is the package that is created in the video showing how to create and use a Swift Package for Swift Package Manager
--- Sources/CTRating/RatingImage.swift
+++ Sources/CTRating/RatingImage.swift
@@ -10,16 +10,23 @@ public enum RatingImage: String {
     case baseball
     case basketball
     case football
+    case custom
     
     var fillImage: Image {
         rawValue.prefix(3) == "SF_" ?
         Image(systemName: String(rawValue.dropFirst(3)) + ".fill")
         :
-        Image("\(rawValue).fill", bundle: .module)
+        rawValue == "custom" ?
+        Image("\(rawValue).fill")
+        :
+        Image("\(rawValue).fill", bundle: .main)
     }
     var openImage: Image {
         rawValue.prefix(3) == "SF_" ?
         Image(systemName: String(rawValue.dropFirst(3)))
+        :
+        rawValue == "custom" ?
+        Image(rawValue)
         :
         Image(rawValue, bundle: .module)
     }
